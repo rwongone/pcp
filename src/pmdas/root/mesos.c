e1383c4b5eb29b377af67c5f2f637bb0918cb28f
Richard Wong <rwong@twitter.com>
2015-10-15 11:32:56 -0700

    fix regcomp method call

--- src/pmdas/root/mesos.c
+++ src/pmdas/root/mesos.c
@@ -19,61 +19,7 @@
 #include "pmda.h"
 
 #include "root.h"
-#include "jsmn.h"
 #include "mesos.h"
-
-/*
- * JSMN helper interfaces for efficiently extracting JSON configs
- */
-
-static int
-jsmneq(const char *js, jsmntok_t *tok, const char *s)
-{
-    if (tok->type != JSMN_STRING)
-		return -1;
-    if (strlen(s) == tok->end - tok->start &&
-	strncasecmp(js + tok->start, s, tok->end - tok->start) == 0)
-		return 0;
-    return -1;
-}
-
-static int
-jsmnflag(const char *js, jsmntok_t *tok, int *bits, int flag)
-{
-    if (tok->type != JSMN_PRIMITIVE)
-		return -1;
-    if (strncmp(js + tok->start, "true", sizeof("true")-1) == 0)
-		*bits |= flag;
-    else
-		*bits &= ~flag;
-    return 0;
-}
-
-static int
-jsmnint(const char *js, jsmntok_t *tok, int *value)
-{
-    char	buffer[64];
-
-    if (tok->type != JSMN_PRIMITIVE)
-		return -1;
-    strncpy(buffer, js + tok->start, tok->end - tok->start);
-    buffer[tok->end - tok->start] = '\0';
-    *value = (int)strtol(buffer, NULL, 0);
-    return 0;
-}
-
-static int
-jsmnstrdup(const char *js, jsmntok_t *tok, char **name)
-{
-    char	*s = *name;
-
-    if (tok->type != JSMN_STRING)
-		return -1;
-    if (s)
-		free(s);
-    s = strndup(js + tok->start, tok->end - tok->start);
-    return ((*name = s) == NULL) ? -1 : 0;
-}
 
 /*
  * Container implementation for the Mesos engine
@@ -193,7 +139,7 @@ mesos_value_refresh(container_engine_t *dp,
 		return -oserror();
 
 	// set values->name
-	if (regcomp(&regex, "[^-]*-[^-]*-(.*)-.*-.*-.*-.*-.*"))
+	if (regcomp(&regex, "[^-]*-[^-]*-(.*)-.*-.*-.*-.*-.*", 0))
 		__pmNotifyErr(LOG_DEBUG, "Failed to compile name regex.\n");
 	if (regexec(&regex, name, maxGroups, matchGroup, 0) == 0) {
 		strncpy(values->name, &name[matchGroup[1].rm_so], 
